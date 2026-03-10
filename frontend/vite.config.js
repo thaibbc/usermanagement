dddc98f3cc33d06bd6a15cc711ad603184a71545
thaibbc <thaibc14@gmail.com>
2026-03-10 21:22:19 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/usermanagement",
+  base:"/",
   server: {
     proxy: {
       '/api': {
